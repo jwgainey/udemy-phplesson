fad2f629ee4753f284a470f7a37284d83c071f4b
jwgainey <jwgainey@hotmail.com>
2020-12-23 22:23:37 -0500

    data table style

--- MYSQL/index.php
+++ MYSQL/index.php
@@ -1,6 +1,52 @@
-<?php 
+<?php
+
 include('connection.php'); 
+echo "<br><br>";
 
-
+$query = "SELECT * FROM users";
+$result = mysqli_query($conn, $query);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>MYSQL</title>
+</head>
+<body>
+
+    <div class="container">
+        <h1>MYSQL Select</h1>
+
+        <?php 
+            if( mysqli_num_rows($result) > 0 ) {
+                // We have Data!
+                // output the data
+            
+                echo "<table class='table table-bordered'><tr><th>ID</th><th>Username</th><th>Email</th></tr>";
+            
+                while($row = mysqli_fetch_assoc($result) ) {
+                    echo "<tr><td>". $row["id"] ."</td><td>". $row["username"] ."</td><td>". $row["email"] ."</td></tr>";
+                }
+            
+                echo "</table>";
+            
+            } else {
+                echo "Whoops! No results.";
+            }
+            
+            mysqli_close($conn);
+        ?>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
